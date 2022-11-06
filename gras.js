cabec7c0e96b47eea34200ae7b6595d0d5d8a74f
Serjik07 <serjik.h1@tumo.org>
2022-11-06 10:57:49 +0400

    inheritance

--- gras.js
+++ gras.js
@@ -1,48 +1,13 @@
-class Grass {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.multiply = 0
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
-    }
-    chooseCell(character) {
-        var found = [];
-        for (let i in this.directions) {
-            let x = this.directions[i][0];
-            let y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == character) {
-                    found.push(this.directions[i]);
-                }
-            }
-        }
-
-        return found;
-    }
+class Grass extends LivingCreature {
 
     mul() {
         this.multiply++;
-        let emptyCells = this.chooseCell(0);
-        let newCell = random(emptyCells);
-
-        if (newCell && this.multiply >= 8) {
-            let newX = newCell[0];
-            let newY = newCell[1];
-            matrix[newY][newX] = 1;
-
-            let newGrass = new Grass(newX, newY, 1);
+        var newCell = random(this.chooseCell(0));
+        if (this.multiply >= 8 && newCell) {
+            var newGrass = new Grass(newCell[0], newCell[1], this.index);
             grassArr.push(newGrass);
+            matrix[newCell[1]][newCell[0]] = this.index;
             this.multiply = 0;
         }
     }
-
 }
